b3878ff60e0a1049097db27fbd985d7956a96a44
PiyushNadoda <910piyushsindhav@gmail.com>
2021-09-18 00:37:15 +0530

    lorem removed

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -7,7 +7,7 @@ import "../Style/home.css";
 import fc from "../Images/illustration-flowing-conversation.svg";
 import gt from "../Images/illustration-grow-together.svg";
 import yu from "../Images/illustration-your-users.svg";
-import im from '../Images/illustration-mockups.svg'
+import im from "../Images/illustration-mockups.svg";
 
 const Home = ({ history }) => {
   const dispatch = useDispatch();
@@ -25,17 +25,15 @@ const Home = ({ history }) => {
       <div>
         <div className="home-container-top">
           <div className="home-container-mockup">
-            <img
-              className="home-mockup"
-              src={im}
-              alt=""
-            />
+            <img className="home-mockup" src={im} alt="" />
             <div className="home-container-mockup-text">
               <h1 className="home-mockup-heading">
-                CREATE SPACE FOR STUDENT TO FIND SOLUTIONS
+                {/* CREATE SPACE FOR STUDENT TO FIND SOLUTIONS */}
+                Space for education
               </h1>
               <p className="home-mockup-subheading">
-              
+                If somebody somewhere has the right answer, suddenly you have it
+                too. Collaborate better in a remote-first world.
               </p>
               {/* <button className="home-mockup-btn">Get Started For Free</button> */}
             </div>
@@ -52,15 +50,15 @@ const Home = ({ history }) => {
           <img className="home-fc-mockup" src={fc} alt="" />
           <h1 className="home-fc-heading">Keep community at our center</h1>
           <p className="home-fc-text">
-            Community is at the heart of everything we do. Nurture healthy communities where everyone is encouraged to learn and give back.
+            Community is at the heart of everything we do. Nurture healthy
+            communities where everyone is encouraged to learn and give back.
           </p>
         </div>
         <div className="home-container-your-users">
           <img className="home-yu-mockup" src={yu} alt="" />
           <h1 className="home-yu-heading">Our Users</h1>
           <p className="home-yu-text">
-            Once signed in to our app,
-            users can start posting immediately.
+            Once signed in to our app, users can start posting immediately.
           </p>
         </div>
       </div>
